289c8a80b2d94b62697fbcfefcab47357191e91b
Moamen <Moamenbekheet98@gmail.com>
2022-08-07 17:26:22 +0200

    done with the number buttons as well as the del and reset buttons

--- script.js
+++ script.js
@@ -18,18 +18,64 @@ function operate(operator, numOne, numTwo){
     if(operator.trim()==='+'){
         return add(numOne,numTwo);
     }
-    else if((operator.trim()==='*')){
+    else if((operator.trim()==='×')){
         return multiply(numOne,numTwo);
     }
     else if(operator.trim()==='-'){
         return subtract(numOne,numTwo);
     }
-    else if(operator.trim()==='/'){
+    else if(operator.trim()==='÷'){
         return divide(numOne,numTwo);
     }
     else{
         //should give out an error
     }
 }
-
+let firstOperand,secondOperand, operator;
 const display = document.querySelector('#screen');
+const oneBtn = document.querySelector('#one');
+const twoBtn = document.querySelector('#two');
+const threeBtn = document.querySelector('#three');
+const fourBtn = document.querySelector('#four');
+const fiveBtn = document.querySelector('#five');
+const sixBtn = document.querySelector('#six');
+const sevenBtn = document.querySelector('#seven');
+const eightBtn = document.querySelector('#eight');
+const nineBtn = document.querySelector('#nine');
+const delBtn = document.querySelector('#del');
+const acBtn = document.querySelector('#ac');
+const multBtn = document.querySelector('#multiplicatoin')
+const eqBtn = document.querySelector('#equals')
+oneBtn.addEventListener('click', function(){
+    display.textContent +='1';
+});
+twoBtn.addEventListener('click', function(){
+    display.textContent +='2';
+});
+threeBtn.addEventListener('click', function(){
+    display.textContent +='3';
+});
+fourBtn.addEventListener('click', function(){
+    display.textContent +='4';
+});
+fiveBtn.addEventListener('click', function(){
+    display.textContent +='5';
+});
+sixBtn.addEventListener('click', function(){
+    display.textContent +='6';
+});
+sevenBtn.addEventListener('click', function(){
+    display.textContent +='7';
+});
+eightBtn.addEventListener('click', function(){
+    display.textContent +='8';
+});
+nineBtn.addEventListener('click', function(){
+    display.textContent +='9';
+});
+delBtn.addEventListener('click',function(){
+    display.textContent = display.textContent.slice(0,display.textContent.length-1);
+})
+acBtn.addEventListener('click',function(){
+    display.textContent = '';
+})
